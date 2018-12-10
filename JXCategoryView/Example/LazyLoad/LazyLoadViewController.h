442669030ff3cfab39e9a94c61f6523cd9dd63f3
pujiaxin33 <317437084@qq.com>
2018-12-10 14:13:11 +0800

    新增列表懒加载LazyLoadViewController示例

---
+++ JXCategoryView/Example/LazyLoad/LazyLoadViewController.h
@@ -0,0 +1,17 @@
+//
+//  LazyLoadViewController.h
+//  JXCategoryView
+//
+//  Created by jiaxin on 2018/12/10.
+//  Copyright © 2018 jiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LazyLoadViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
