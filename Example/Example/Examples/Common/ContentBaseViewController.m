16955825617cf71757c0522c08f2845c3d168eab
liangsen2 <liangsende1@dingtalk.com>
2020-09-28 14:24:33 +0800

    使用JXCategoryListContainerType_CollectionView没毛病

--- Example/Example/Examples/Common/ContentBaseViewController.m
+++ Example/Example/Examples/Common/ContentBaseViewController.m
@@ -70,7 +70,7 @@
 
 - (JXCategoryListContainerView *)listContainerView {
     if (_listContainerView == nil) {
-        _listContainerView = [[JXCategoryListContainerView alloc] initWithType:JXCategoryListContainerType_ScrollView delegate:self];
+        _listContainerView = [[JXCategoryListContainerView alloc] initWithType:JXCategoryListContainerType_CollectionView delegate:self];
     }
     return _listContainerView;
 }
