a41ba593ec2fdf68f50834534aed6700447c6e9b
monkey <317437084@qq.com>
2023-12-03 11:57:59 +0800

    更新demo导航栏样式

--- Example/Example/AppDelegate.m
+++ Example/Example/AppDelegate.m
@@ -12,7 +12,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self setupNavigationBar];
     return YES;
+}
+
+- (void)setupNavigationBar {
+    if (@available(iOS 13.0, *)) {
+        UINavigationBarAppearance *appearance = [[UINavigationBarAppearance alloc] init];
+        [appearance configureWithOpaqueBackground];
+        [UINavigationBar appearance].standardAppearance = appearance;
+        [UINavigationBar appearance].scrollEdgeAppearance = appearance;
+    }
 }
 
 @end
