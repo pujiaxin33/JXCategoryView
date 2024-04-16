782e1c3b7d19ed9e44799af47de0b41d748d9933
monkey <317437084@qq.com>
2024-04-16 16:38:13 +0800

    support Carthage

--- JXCategoryView/JXCategoryView.h
+++ JXCategoryView/JXCategoryView.h
@@ -16,4 +16,23 @@ FOUNDATION_EXPORT const unsigned char JXCategoryViewVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JXCategoryView/PublicHeader.h>
 
+#import "JXCategoryBaseView.h"
+#import "JXCategoryIndicatorView.h"
+#import "JXCategoryTitleView.h"
+#import "JXCategoryImageView.h"
+#import "JXCategoryTitleImageView.h"
+#import "JXCategoryNumberView.h"
+#import "JXCategoryDotView.h"
 
+#import "JXCategoryFactory.h"
+#import "JXCategoryIndicatorProtocol.h"
+#import "JXCategoryViewDefines.h"
+#import "JXCategoryListContainerView.h"
+
+#import "JXCategoryIndicatorComponentView.h"
+#import "JXCategoryIndicatorLineView.h"
+#import "JXCategoryIndicatorTriangleView.h"
+#import "JXCategoryIndicatorImageView.h"
+#import "JXCategoryIndicatorBackgroundView.h"
+#import "JXCategoryIndicatorBallView.h"
+#import "JXCategoryIndicatorRainbowLineView.h"
